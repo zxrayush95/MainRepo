5e0aca01134f920e3b4b215a954aaf777e097d26
zxrayush95 <tyren35678@gmail.com>
2026-01-22 01:09:37 +0530

    Update RRef.js

--- RRef.js
+++ RRef.js
@@ -1,5 +1,5 @@
 {
-  "version": "5.7",
+  "version": "5.8",
   "url": "https://github.com/ZeroEngine123/Reseller-WallHack/raw/refs/heads/main/libReseller-Admin-Wh.so",
   "libLink": "https://github.com/ZeroEngine123/Reseller-WallHack/raw/refs/heads/main/libReseller-Admin-Wh.so"
 }
